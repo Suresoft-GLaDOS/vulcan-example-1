2786a35000748b129c55d83cd1dafe006092d794
HansolChoe <hansol614@gmail.com>
2022-05-26 13:51:46 +0900

    Add a bug

--- example.c
+++ example.c
@@ -6,7 +6,7 @@ int mid(int x, int y, int z){
         if (x < y) {
             ret = y;
         } else if (x < z) {
-            ret = y; // buggy line
+            ret = y; // buggy line!
         }
     } else {
         if (x > y) {

c921b13738bd9fc2cd62485d8cc00c3ec7b2afa5
vulcan-action <vulcan@action>
2022-05-26 06:18:50 +0000

    Committed the automatically generated patch

--- example.c
+++ example.c
@@ -6,7 +6,8 @@ int mid(int x, int y, int z){
         if (x < y) {
             ret = y;
         } else if (x < z) {
-            ret = y; // buggy line!
+            ret = (y * x);
+ // buggy line!
         }
     } else {
         if (x > y) {
